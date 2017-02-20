e7b17c691b4d8f27279380b00c35ad2468f5ab81
lomadi <lomadi@mac.com>
2017-02-20 02:28:55 +0100

    update docker file

--- commands-repository.sh
+++ commands-repository.sh
@@ -4,26 +4,25 @@ docker rmi $(docker images -q)
 docker ps -aq
 docker rm  $(docker ps -aq)
 
-docker build -t lomadi/webtrees .
-docker build -t lomadi/webtrees  images/webtrees
+docker build -t lomadi/imdbpy .
 
-docker run -it --rm --name webtrees  lomadi/webtrees
+docker run -it --rm --name imdbpy  lomadi/imdbpy
 
 docker-compose up -d
 docker-compose down
 
-docker logs webtrees
-docker exec -it webtrees /bin/bash
+docker logs imdbpy
+docker exec -it imdbpy /bin/bash
 
-sudo du -d 2 -h /var/lib/docker/devicemapper | grep 'docker inspect -f "{{.Id}}" webtrees'
+sudo du -d 2 -h /var/lib/docker/devicemapper | grep 'docker inspect -f "{{.Id}}" imdbpy'
 
 
 # DB 
-docker exec -it webtrees-database /bin/bash
+docker exec -it imdbpy-database /bin/bash
 mysqldump --all-databases > dump.sql
-mysql -u webtrees -p  
+mysql -u imdbpy -p  
 
-use webtrees;
+use imdbpy;
 show tables;
-select * from wt_user ;
+
 
